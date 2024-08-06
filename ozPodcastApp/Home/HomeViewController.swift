ed1e2cd19875f95a7f656820a486a6a9df7432e0
vb10 <vbacik.10@gmail.com>
2024-08-07 02:27:31 +0300

    added core library packages in the project #1

--- ozPodcastApp/Home/HomeViewController.swift
+++ ozPodcastApp/Home/HomeViewController.swift
@@ -3,23 +3,25 @@
 //  ozPodcastApp
 //
 //  Created by vb10 on 7.08.2024.
-//  
+//
 //
 
 import UIKit
 
 class HomeViewController: UIViewController {
-    
     // MARK: - Lifecycle Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        print("aa")
     }
 
     // MARK: - Properties
+
     var presenter: ViewToPresenterHomeProtocol?
-    
 }
 
-extension HomeViewController: PresenterToViewHomeProtocol{
+extension HomeViewController: PresenterToViewHomeProtocol {
     // TODO: Implement View Output Methods
 }
