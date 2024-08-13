a3efe11b6cf7a8e3e43053689569373373f224b2
vb10 <vbacik.10@gmail.com>
2024-08-14 01:44:58 +0300

    added folder structure with viper template inform

--- ozPodcastApp/Features/Home/HomeInteractor.swift
+++ ozPodcastApp/Features/Home/HomeInteractor.swift
@@ -2,13 +2,17 @@
 //  HomeInteractor.swift
 //  ozPodcastApp
 //
-//  Created by vb10 on 7.08.2024.
+//  Created by vb10 on 14.08.2024.
 //  
 //
 
 import Foundation
 
 class HomeInteractor: PresenterToInteractorHomeProtocol {
+    func saveLikeCountInDatabase() {
+        /// use shared 
+    }
+    
 
     // MARK: Properties
     var presenter: InteractorToPresenterHomeProtocol?
--- ozPodcastApp/Features/Home/HomePresenter.swift
+++ ozPodcastApp/Features/Home/HomePresenter.swift
@@ -2,20 +2,26 @@
 //  HomePresenter.swift
 //  ozPodcastApp
 //
-//  Created by vb10 on 7.08.2024.
-//  
+//  Created by vb10 on 14.08.2024.
+//
 //
 
 import Foundation
 
 class HomePresenter: ViewToPresenterHomeProtocol {
-
     // MARK: Properties
+
     var view: PresenterToViewHomeProtocol?
     var interactor: PresenterToInteractorHomeProtocol?
     var router: PresenterToRouterHomeProtocol?
+
+    func onLikPressed() {
+        interactor?.saveLikeCountInDatabase()
+    }
 }
 
 extension HomePresenter: InteractorToPresenterHomeProtocol {
-    
+    func showSuccsessMessage(message: String) {
+        view?.showDialogMessage(message: message)
+    }
 }
